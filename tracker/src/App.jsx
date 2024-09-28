7725ab9674148aa67f8952bdb96586ef70aa127c
Hrithik Reddy <158307841+HrithikD1@users.noreply.github.com>
2024-09-28 04:48:14 +0000

    First

---
+++ tracker/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header id='header' className='w-300 h-300 flex content-center bg-black text-white'>
+        <h1 className="font-sans text-9x1">Tracker</h1>
+        {/*In Progress*/}
+      </header>
+      <main className='body'>
+        <div>Todays's work</div>
+        <table>
+        </table>
+      </main>
+    </>
+  )
+}
+
+export default App
